543c6afd2f6432f2422f3c5ce335bdf78e288b27
Prashant Kulkarni <39596574+prashantkul@users.noreply.github.com>
2020-04-21 15:03:38 -0700

    Updating to CIS1.1 (#20)
    * Updating controls 6.x

--- controls/6.03-db.rb
+++ controls/6.03-db.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-title 'Ensure that MySql database instance does not allow anyone to connect with administrative privileges.'
+title 'Ensure that Cloud SQL Server database Instances are secure'
 
 gcp_project_id = attribute('gcp_project_id')
 cis_version = attribute('cis_version')
@@ -21,31 +21,74 @@ cis_url = attribute('cis_url')
 control_id = "6.3"
 control_abbrev = "db"
 
-control "cis-gcp-#{control_id}-#{control_abbrev}" do
-  impact 1.0
+# 6.3.1
+sub_control_id = "#{control_id}.1"
+control "cis-gcp-#{sub_control_id}-#{control_abbrev}" do
+  impact 1.1
 
-  title "[#{control_abbrev.upcase}] Ensure that MySql database instance does not allow anyone to connect with administrative privileges."
+  title "[#{control_abbrev.upcase}] Ensure that the 'cross db ownership chaining' database flag for Cloud SQL Server instance is set to 'off'"
 
-  desc "It is recommended to set a password for the administrative user (root by default) to prevent unauthorized access to the SQL database Instances.
-
-This recommendation is applicable only for MySql Instances. PostgreSQL does not offer any setting for No Password from cloud console."
-  desc "rationale", "At the time of MySql Instance creation, not providing a administrative password allows anyone to connect to the SQL database instance with administrative privileges. Root password should be set to ensure only authorized users have these privileges."
+  desc "It is recommended to set cross db ownership chaining database flag for Cloud SQL SQL Server instance to off. "
+  desc "rationale", "Use the cross db ownership for chaining option to configure cross-database ownership chaining for an instance of 
+                     Microsoft SQL Server. This server option allows you to control cross-database ownership chaining at the database level or 
+                     to allow cross-database ownership chaining for all databases.Enabling cross db ownership is not recommended unless all of the 
+                     databases hosted by the instance of SQL Server must participate in cross-database ownership chaining and you are aware of the 
+                     security implications of this setting"
 
   tag cis_scored: true
   tag cis_level: 1
-  tag cis_gcp: "#{control_id}"
+  tag cis_gcp: "#{sub_control_id}"
   tag cis_version: "#{cis_version}"
   tag project: "#{gcp_project_id}"
 
   ref "CIS Benchmark", url: "#{cis_url}"
-  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/mysql/create-manage-users"
-  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/mysql/create-instance"
+  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/sqlserver/flags"
+  ref "GCP Docs", url: "https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/cross-db-ownership-chaining-server-configuration-option?view=sql-server-ver15"
 
-  describe "Not scored" do
-    before do
-      skip
+  google_sql_database_instances(project: gcp_project_id).instance_names.each do |db|
+    describe.one do
+      google_sql_database_instance(project: gcp_project_id, database: db).settings.database_flags.each do |flag|
+        describe flag.item do
+          it { should include(:name => 'cross db ownership chaining') }
+          it { should include(:value => 'off') }
+        end
+      end
     end
-    it {should eq "Not scored"}
-  end
+  end 
+end 
 
-end
+# 6.3.2
+sub_control_id = "#{control_id}.2"
+control "cis-gcp-#{sub_control_id}-#{control_abbrev}" do
+  impact 1.1
+
+  title "[#{control_abbrev.upcase}] Ensure that the 'contained database authentication' database flag for Cloud SQL server instance is set to 'off'"
+
+  desc "It is recommended to set contained database authentication database flag for Cloud SQL on the SQL Server instance is set to off."
+  desc "rationale", "A contained database includes all database settings and metadata required to define the database and has no configuration 
+                    dependencies on the instance of the Database Engine where the database is installed. Users can connect to the database without authenticating 
+                    a login at the Database Engine level. Isolating the database from the Database Engine makes it possible to easily move the database to another 
+                    instance of SQL Server."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gcp: "#{sub_control_id}"
+  tag cis_version: "#{cis_version}"
+  tag project: "#{gcp_project_id}"
+
+  ref "CIS Benchmark", url: "#{cis_url}"
+  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/sqlserver/flags"
+  ref "GCP Docs", url: "https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/contained-database-authentication-server-configuration-option?view=sql-server-ver15"
+  ref "GCP Docs", url: "https://docs.microsoft.com/en-us/sql/relational-databases/databases/security-best-practices-with-contained-databases?view=sql-server-ver15"
+
+  google_sql_database_instances(project: gcp_project_id).instance_names.each do |db|
+    describe.one do
+      google_sql_database_instance(project: gcp_project_id, database: db).settings.database_flags.each do |flag|
+        describe flag.item do
+          it { should include(:name => 'contained database authentication') }
+          it { should include(:value => 'off') }
+        end
+      end
+    end
+  end 
+end 
--- controls/6.05-db.rb
+++ controls/6.05-db.rb
@@ -13,21 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-title 'Ensure that Cloud SQL database instance requires all incoming connections to use SSL'
+title 'Database Server should accept connections only from trusted Network(s)/IP(s) and restrict access from the world'
 
 gcp_project_id = attribute('gcp_project_id')
 cis_version = attribute('cis_version')
 cis_url = attribute('cis_url')
-control_id = "6.4"
+control_id = "6.5"
 control_abbrev = "db"
 
 control "cis-gcp-#{control_id}-#{control_abbrev}" do
   impact 1.0
 
-  title "[#{control_abbrev.upcase}] Ensure that Cloud SQL database instance requires all incoming connections to use SSL"
+  title "[#{control_abbrev.upcase}] Database Server should accept connections only from trusted Network(s)/IP(s) and restrict access from the world"
 
-  desc "It is recommended to enforce all incoming connections to SQL database instance to use SSL."
-  desc "rationale", "SQL database connections if successfully trapped (MITM); can reveal sensitive data like credentials, database queries, query outputs etc. For security, it is recommended to always use SSL encryption when connecting to your instance. This recommendation is applicable for Postgresql, MySql generation 1 and MySql generation 2 Instances."
+  desc "Database Server should accept connections only from trusted Network(s)/IP(s) and restrict access from the world."
+  desc "rationale", "To minimize attack surface on a Database server instance, only trusted/known and required IP(s) should be white-listed to connect to it."
 
   tag cis_scored: true
   tag cis_level: 1
@@ -36,12 +36,12 @@ control "cis-gcp-#{control_id}-#{control_abbrev}" do
   tag project: "#{gcp_project_id}"
 
   ref "CIS Benchmark", url: "#{cis_url}"
-  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/postgres/configure-ssl-instance"
+  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/mysql/configure-ip"
 
   google_sql_database_instances(project: gcp_project_id).instance_names.each do |db|
-    describe "[#{gcp_project_id}] CloudSQL #{db}" do
-      subject { google_sql_database_instance(project: gcp_project_id, database: db) }
-      it { should have_ip_configuration_require_ssl }
-    end
+      describe "[#{gcp_project_id}] CloudSQL #{db}" do
+        subject { google_sql_database_instance(project: gcp_project_id, database: db).authorized_networks }
+        it { should_not include('0.0.0.0/0') }
+     end
   end
 end
