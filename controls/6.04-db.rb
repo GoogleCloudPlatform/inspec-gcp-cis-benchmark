22ae9509df70b85676883bcad21914e90ab28d75
Aayush Srivastava <aayu@google.com>
2020-04-24 17:44:31 -0400

    fixes 6.04

---
+++ controls/6.04-db.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+# Copyright 2019 The inspec-gcp-cis-benchmark Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+title 'Ensure that Cloud SQL database instance requires all incoming connections to use SSL'
+
+gcp_project_id = attribute('gcp_project_id')
+cis_version = attribute('cis_version')
+cis_url = attribute('cis_url')
+control_id = "6.4"
+control_abbrev = "db"
+
+control "cis-gcp-#{control_id}-#{control_abbrev}" do
+  impact 1.0
+
+  title "[#{control_abbrev.upcase}] Ensure that Cloud SQL database instance requires all incoming connections to use SSL"
+
+  desc "It is recommended to enforce all incoming connections to SQL database instance to use SSL."
+  desc "rationale", "SQL database connections if successfully trapped (MITM); can reveal sensitive data like credentials, database queries, query outputs etc. For security, it is recommended to always use SSL encryption when connecting to your instance. This recommendation is applicable for Postgresql, MySql generation 1 and MySql generation 2 Instances."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gcp: "#{control_id}"
+  tag cis_version: "#{cis_version}"
+  tag project: "#{gcp_project_id}"
+
+  ref "CIS Benchmark", url: "#{cis_url}"
+  ref "GCP Docs", url: "https://cloud.google.com/sql/docs/postgres/configure-ssl-instance"
+
+  google_sql_database_instances(project: gcp_project_id).instance_names.each do |db|
+    describe "[#{gcp_project_id}] CloudSQL #{db}" do
+      subject { google_sql_database_instance(project: gcp_project_id, database: db) }
+      it { should have_ip_configuration_require_ssl }
+    end
+  end
+end
