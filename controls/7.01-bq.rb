b7bbc7b8245f1d715605739b9de81558575dc767
Konrad Schieban <kschieban@google.com>
2021-12-19 19:02:38 +0100

    CIS 1.2: Adding 7.2

--- controls/7.01-bq.rb
+++ controls/7.01-bq.rb
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# export GOOGLE_APPLICATION_CREDENTIALS="/Users/aayu/dev/inspec-gcp-cis-benchmark/inspec-key.json"
 
 title 'Ensure that BigQuery datasets are not anonymously or publicly accessible'
 
