25183ecba0d9fcb9296654c7d401d29bd559320a
Alexander Bilz <mail@alexbilz.com>
2025-06-06 12:22:30 +0000

    fix: make control 1.2 unscored

--- controls/1.02-iam.rb
+++ controls/1.02-iam.rb
@@ -1,3 +1,5 @@
+# controls/1.02-iam.rb (assuming 1.02 is the actual file name)
+
 # Copyright 2019 The inspec-gcp-cis-benchmark Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +19,7 @@ title 'Ensure that multi-factor authentication is enabled for all non-service ac
 gcp_project_id = input('gcp_project_id')
 cis_version = input('cis_version')
 cis_url = input('cis_url')
-control_id = '1.2'
+control_id = '1.2' # This should match the actual control ID
 control_abbrev = 'iam'
 
 control "cis-gcp-#{control_id}-#{control_abbrev}" do
@@ -25,8 +27,9 @@ control "cis-gcp-#{control_id}-#{control_abbrev}" do
   title "[#{control_abbrev.upcase}] Ensure that multi-factor authentication is enabled for all non-service accounts"
   desc 'Setup multi-factor authentication for Google Cloud Platform accounts.'
   desc 'rationale', 'Multi-factor authentication requires more than one mechanism to authenticate a user. This secures user logins from attackers exploiting stolen or weak credentials.'
+  desc 'notes', 'This control generally requires manual verification because InSpec does not directly query user MFA status from Cloud Identity or Google Workspace. Verification steps are provided in the references.'
 
-  tag cis_scored: false
+  tag cis_scored: false # Keep this as false
   tag cis_level: 1
   tag cis_gcp: control_id.to_s
   tag cis_version: cis_version.to_s
@@ -37,18 +40,7 @@ control "cis-gcp-#{control_id}-#{control_abbrev}" do
   ref 'GCP Docs', url: 'https://cloud.google.com/solutions/securing-gcp-account-u2f'
   ref 'GCP Docs', url: 'https://support.google.com/accounts/answer/185839'
 
-  # Iterate over all users in the project.  google_iam_users returns an array of user objects.
-  google_iam_users(parent: "projects/#{gcp_project_id}").user_ids.each do |user_id|
-    user = google_iam_user(name: user_id)
-
-    # Skip service accounts
-    next if user_id.end_with?('@gserviceaccount.com')
-
-    describe "[#{gcp_project_id}] User #{user.name}" do
-      subject { user }
-      # Check if MFA is enabled
-      it { should exist }
-      its('mfa_enabled?') { should be true }
-    end
+  describe 'This control is not scored' do
+    skip 'This control is not scored'
   end
 end
